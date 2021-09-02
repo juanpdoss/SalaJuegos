8a929cd0682a19e36abebb67c9eba3b24f8356e1
juanpdoss <juanpdoss@gmail.com>
2021-09-02 10:07:06 -0300

    agrego mas estilos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ErrorComponent } from './error/error.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { QuienSoyComponent } from './quien-soy/quien-soy.component';
 
+//defino las rutas
 const routes: Routes = [
-  { path: 'Home', component: HomeComponent },
-  { path: 'Login', component: LoginComponent },
-  { path: 'AboutMe', component: QuienSoyComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'aboutMe', component: QuienSoyComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '**', component: ErrorComponent },
 ];
+//asterisco asterisco que no sea vacio no es login no es home, pa el error
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 import { QuienSoyComponent } from './quien-soy/quien-soy.component';
+import { ErrorComponent } from './error/error.component';
 
 @NgModule({
   declarations: [
@@ -13,6 +13,7 @@ import { QuienSoyComponent } from './quien-soy/quien-soy.component';
     LoginComponent,
     HomeComponent,
     QuienSoyComponent,
+    ErrorComponent,
   ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
